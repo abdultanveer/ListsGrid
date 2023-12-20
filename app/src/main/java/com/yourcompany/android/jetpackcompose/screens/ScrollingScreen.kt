9966aed5bf784075f7b1a6c6f5b3e48cecff56c8
Abdul <abdul.tanveer@gmail.com>
2023-12-20 09:43:24 +0800

    horizontal scrolling

--- app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ScrollingScreen.kt
+++ app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ScrollingScreen.kt
@@ -37,7 +37,9 @@ package com.yourcompany.android.jetpackcompose.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -64,7 +66,7 @@ fun ScrollingScreen() {
 
 @Composable
 fun MyScrollingScreen(modifier:Modifier = Modifier) {
-  Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+  Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
     BookImage(imageResId = R.drawable.advanced_architecture_android, contentDescriptionResId = R.string.advanced_architecture_android)
     BookImage(imageResId = R.drawable.kotlin_aprentice, contentDescriptionResId = R.string.kotlin_apprentice)
     BookImage(imageResId = R.drawable.kotlin_coroutines, contentDescriptionResId = R.string.kotlin_coroutines )
